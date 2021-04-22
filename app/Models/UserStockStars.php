dcc9e8303450bc0f00f44f40ab53aaebb923db34
张宏文 <zhanghongwen01@bilibili.com>
2021-04-22 23:04:54 +0800

    a

--- app/Models/UserStockStars.php
+++ app/Models/UserStockStars.php
@@ -16,6 +16,10 @@ class UserStockStars extends baseModel
 
     public function rateFromStar(): float
     {
-        return sprintf("%.4f", ($this->star_price - $this->myStock()->close) / $this->myStock()->close);
+        if ($this->myStock()->close > 0) {
+            return sprintf("%.4f", ($this->star_price - $this->myStock()->close) / $this->myStock()->close);
+        } else {
+            return 0.0;
+        }
     }
 }
