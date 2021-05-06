68296c17934abff16580b3865b5aa5dc156989c8
张宏文 <zhanghongwen01@bilibili.com>
2021-05-06 19:36:21 +0800

    add order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use \App\Models\User;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function list(Request $request)
+    {
+        $m = new Order();
+        $all = $request->all();
+        if (isset($all["keyword"])) {
+            $m = $m->where("symbol", "like", "%" . $all['keyword'] . "%");
+        }
+        $ls = $m->where("user_id", $_SESSION['user_id'])->orderBy('ctime', 'desc')->paginate(2);
+        return view("order.list", ["ls" => $ls, "all" => $all]);
+    }
+
+    public function put(Request $request)
+    {
+        $user = User::find($_SESSION['user_id']);
+        if (is_null($user) || $user->appkey == "") {
+            return $this->json(300, "还未绑定appkey，请先去绑定");
+        }
+    }
 }
